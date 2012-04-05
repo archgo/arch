b6ed8227bbdac77b22a0a2d4eb9bf1181db7dd14
arch90 <arch90@gmail.com>
2012-04-05 13:21:47 +0800

    hello

---
+++ archi.cpp
@@ -0,0 +1,33 @@
+truct test1
+{
+	int a;
+	char b;
+};
+
+class base
+{()
+	public:
+		int get_test();
+		void set_test(int i);
+main(a
+	private:
+		int t1;
+};:{[()]}
+a0
+int base ::get_test()
+{
+	return 0;
+}               0
+
+ma   ma  
+void base::set_test(int i)
+{
+	t1=i;
+}
+
+int test();
+{
+	base *b;
+	struct test1;Button1_Click
+
+}
